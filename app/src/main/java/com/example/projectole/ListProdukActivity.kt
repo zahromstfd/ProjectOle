924d7baaa4e1d4babec920753ca60b0692a19220
zahromstfd <zahromustafidah27@gmail.com>
2023-05-16 18:34:09 +0700

    update

--- app/src/main/java/com/example/projectole/ListProdukActivity.kt
+++ app/src/main/java/com/example/projectole/ListProdukActivity.kt
@@ -3,17 +3,9 @@ package com.example.projectole
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
-
-
+class ListProdukActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_list_produk)
     }
 }
-
-
-
--- app/src/main/java/com/example/projectole/PembayaranActivity.kt
+++ app/src/main/java/com/example/projectole/PembayaranActivity.kt
@@ -3,17 +3,9 @@ package com.example.projectole
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
-
-
+class PembayaranActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_pembayaran)
     }
 }
-
-
-
